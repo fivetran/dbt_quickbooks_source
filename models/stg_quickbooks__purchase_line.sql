4b6ab89e4d3ad898420e7a041e2a4341cc8975e9
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2021-02-09 10:16:32 -0600

    circle updates

--- models/stg_quickbooks__purchase_line.sql
+++ models/stg_quickbooks__purchase_line.sql
@@ -10,8 +10,8 @@ fields as (
     select
         /*
         The below macro is used to generate the correct SQL for package staging models. It takes a list of columns
-        that are expected/needed (staging_columns from dbt_salesforce_source/models/tmp/) and compares it with columns
-        in the source (source_columns from dbt_salesforce_source/macros/).
+        that are expected/needed (staging_columns from dbt_quickbooks_source/models/tmp/) and compares it with columns
+        in the source (source_columns from dbt_quickbooks_source/macros/).
         For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
         */
 
